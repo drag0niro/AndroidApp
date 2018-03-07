d4e5f62c2825d4ce5b79026b1c234858dcaa0c26
Damian Drożdż <damiandrozdz@interia.pl>
2018-03-07 15:18:57 +0100

    + menu

--- app/src/main/java/pl/derankingteam/myapplication/MainActivity.java
+++ app/src/main/java/pl/derankingteam/myapplication/MainActivity.java
@@ -4,31 +4,47 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutCompat;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.GridLayout;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private DrawerLayout mDrawerLayout;
-    private ActionBarDrawerToggle mToggle;
+    GridLayout mainGrid;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
- /**       mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer);
-        mToggle = new ActionBarDrawerToggle(this,mDrawerLayout,R.string.open,R.string.close);
-        mDrawerLayout.addDrawerListener(mToggle);
-        mToggle.syncState();
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-  **/
+        setContentView(R.layout.activity_main2);
+
+        mainGrid = (GridLayout)findViewById(R.id.mainGrid);
+
+        setSingleEvent (mainGrid);
     }
-/**
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item)
+
+    private void setToggleEvent (GridLayout mainGrid)
     {
-        if (mToggle.onOptionsItemSelected(item))
-            return true;
-        return super.onOptionsItemSelected(item);
+
     }
-**/
+
+    private void setSingleEvent (GridLayout mainGrid)
+    {
+        for (int i = 0; i < mainGrid.getChildCount(); i++)
+        {
+            LinearLayoutCompat linearLayoutCompat = (LinearLayoutCompat) mainGrid.getChildAt(i);
+            final int finalT = i;
+            linearLayoutCompat.setOnClickListener(new View.OnClickListener()
+                {
+                    @Override
+                    public void onClick(View v)
+                    {
+                        Toast.makeText(MainActivity.this, "Clicked at index " + finalT, Toast.LENGTH_SHORT).show();
+                    }
+                });
+        }
+    }
 }
